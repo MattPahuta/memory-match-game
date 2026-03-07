19e5336fcaa25967648c77258ca4548ed5459ecf
Matt Pahuta <mhpahuta@gmail.com>
2026-03-07 15:15:24 -0800

    Add A11Y update to GameOver component

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,8 +1,17 @@
 import Button from "./Button";
+import { useRef, useEffect } from "react";
+
 
 function GameOver({ handleClick }) {
+  
+  const gameOverRef = useRef(null);
+  
+  useEffect(() => {
+    gameOverRef.current.focus();
+  }, [])
+
   return (
-    <div>
+    <div ref={gameOverRef} tabIndex={-1}>
       <p>You've matched all the cards!</p>
       <Button handleClick={handleClick}>
         Play again
